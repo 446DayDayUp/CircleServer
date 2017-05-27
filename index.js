b7577397465f922e9d113f614ddc6b1aa2e5377d
Jing Tao <genetao06@hotmail.com>
2017-05-27 15:40:15 -0400

    Modify port and add socket.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 var express = require('express')
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 var app = express()
 
 app.get('/', function (req, res) {
   res.send('hello')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!')
 })
