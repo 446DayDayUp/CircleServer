9f2af9ac5cc4597357978b60b3c4a515ab27284a
Jing Tao <genetao06@hotmail.com>
2017-06-22 20:03:00 -0400

    Nit.

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get('/get-chat-room', (req, res) => {
     function (err, chatRoom) {
       if (err) res.status(500).send(err.toString());
       res.send(chatRoom);
-    });
+    })
   }
 });
 
