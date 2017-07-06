a1c79f22de4acc3c7d9c445cf73d2f63a830df7c
Jing Tao <genetao06@hotmail.com>
2017-07-06 18:53:02 -0400

    Fix bug.

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ app.post('/create-chat-room', function(req, res){
   });
 });
 
-app.post('upload-image', function(req, res) {
+app.post('/upload-image', function(req, res) {
   console.log("upload-image", req.body);
 })
 
